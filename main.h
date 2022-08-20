f24160e8bd2d5f878984787d8e499b712ab33ad5
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-08-20 22:17:00 +0800

    bml+ compatibility

--- main.h
+++ main.h
@@ -1,27 +1,29 @@
 #pragma once
 
-#include <BML/BMLAll.h>
+#include <BMLPlus/BMLAll.h>
 
 extern "C" {
-	__declspec(dllexport) IMod* BMLEntry(IBML* bml);
+  __declspec(dllexport) IMod* BMLEntry(IBML* bml);
 }
 
+typedef const char* C_CKSTRING;
+
 class SpeedrunShortcuts : public IMod {
-	CKDataArray* current_level;
-	InputHook* input_manager = m_bml->GetInputManager();
+  CKDataArray* current_level;
+  InputHook* input_manager = m_bml->GetInputManager();
   CK_ID esc_event, restart_level;
 public:
-	SpeedrunShortcuts(IBML* bml) : IMod(bml) {}
+  SpeedrunShortcuts(IBML* bml) : IMod(bml) {}
 
-	virtual CKSTRING GetID() override { return "SpeedrunShortcuts"; }
-	virtual CKSTRING GetVersion() override { return "0.0.2"; }
-	virtual CKSTRING GetName() override { return "Speedrun Shortcuts"; }
-	virtual CKSTRING GetAuthor() override { return "BallanceBug"; }
-	virtual CKSTRING GetDescription() override { return "Provides keyboard shortcuts for speedrunning."; }
-	DECLARE_BML_VERSION;
+  virtual C_CKSTRING GetID() override { return "SpeedrunShortcuts"; }
+  virtual C_CKSTRING GetVersion() override { return "0.0.2"; }
+  virtual C_CKSTRING GetName() override { return "Speedrun Shortcuts"; }
+  virtual C_CKSTRING GetAuthor() override { return "BallanceBug"; }
+  virtual C_CKSTRING GetDescription() override { return "Provides keyboard shortcuts for speedrunning."; }
+  DECLARE_BML_VERSION;
   
   void OnProcess() override;
-  void OnLoadScript(CKSTRING filename, CKBehavior* script) override;
+  void OnLoadScript(C_CKSTRING filename, CKBehavior* script) override;
 
   void ResetBall(CKContext* ctx);
 };
