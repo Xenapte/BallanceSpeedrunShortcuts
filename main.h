e14eb6e637bd988d314a2d803b68b06d1e9a80b7
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-12-30 17:25:23 +0800

    add tests for BMMO client

--- main.h
+++ main.h
@@ -1,27 +1,42 @@
 #pragma once
 
 #include <BML/BMLAll.h>
+#include "client.h"
 
 extern "C" {
 	__declspec(dllexport) IMod* BMLEntry(IBML* bml);
 }
 
 class SpeedrunShortcuts : public IMod {
-	CKDataArray* current_level;
+	CKDataArray* current_level{};
 	InputHook* input_manager = m_bml->GetInputManager();
-  CK_ID esc_event, restart_level;
+  CK_ID esc_event{}, restart_level{};
+	bool init = false, nav_inactive = true;
+	IProperty* props[3]{}; // cheat, spawn, reset
+	CKKEYBOARD keys[3]{}; // cheat, spawn, reset
+	client* mmo_client = nullptr;
+	IProperty* prop_mmo_strict_mode{};
+	bool mmo_strict_mode = false;
+
+	void update_key_property(int index);
+
 public:
 	SpeedrunShortcuts(IBML* bml) : IMod(bml) {}
 
 	virtual CKSTRING GetID() override { return "SpeedrunShortcuts"; }
-	virtual CKSTRING GetVersion() override { return "0.0.2"; }
+	virtual CKSTRING GetVersion() override { return "0.1.0"; }
 	virtual CKSTRING GetName() override { return "Speedrun Shortcuts"; }
 	virtual CKSTRING GetAuthor() override { return "BallanceBug"; }
 	virtual CKSTRING GetDescription() override { return "Provides keyboard shortcuts for speedrunning."; }
 	DECLARE_BML_VERSION;
   
   void OnProcess() override;
+	void OnPostStartMenu() override;
   void OnLoadScript(CKSTRING filename, CKBehavior* script) override;
+	void OnBallNavInactive() override;
+	void OnBallNavActive() override;
+	void OnLoad() override;
+	void OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) override;
 
-  void ResetBall(CKContext* ctx);
+  // void ResetBall(CKContext* ctx);
 };
