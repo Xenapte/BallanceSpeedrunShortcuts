59e8d3977c8232b7cd01f678d4d88f379809c001
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-12-30 17:47:16 +0800

    fix indentation

--- main.cpp
+++ main.cpp
@@ -5,130 +5,130 @@ IMod* BMLEntry(IBML* bml) {
 }
 
 void SpeedrunShortcuts::OnLoadScript(CKSTRING filename, CKBehavior* script) {
-	if (strcmp(script->GetName(), "Gameplay_Events") == 0) {
-		auto* esc = ScriptHelper::FindFirstBB(script, "Key Event");
-		esc_event = CKOBJID(esc->GetOutput(0));
+  if (strcmp(script->GetName(), "Gameplay_Events") == 0) {
+    auto* esc = ScriptHelper::FindFirstBB(script, "Key Event");
+    esc_event = CKOBJID(esc->GetOutput(0));
   }
-	if (strcmp(script->GetName(), "Menu_Pause") == 0) {
-		restart_level = CKOBJID(ScriptHelper::FindFirstBB(script, "Restart Level"));
-	}
+  if (strcmp(script->GetName(), "Menu_Pause") == 0) {
+    restart_level = CKOBJID(ScriptHelper::FindFirstBB(script, "Restart Level"));
+  }
 }
 
 void SpeedrunShortcuts::OnBallNavInactive() { nav_inactive = true; }
 void SpeedrunShortcuts::OnBallNavActive() { nav_inactive = false; }
 
 void SpeedrunShortcuts::OnLoad() {
-	GetConfig()->SetCategoryComment("Keys", "Set the keys used to trigger actions. Must be used with the Alt key.");
-	auto tmp_prop = GetConfig()->GetProperty("Keys", "CheatToggle");
-	tmp_prop->SetDefaultKey(CKKEY_C);
-	tmp_prop->SetComment("The key for toggling Cheat Mode.");
-	props[0] = tmp_prop;
-	tmp_prop = GetConfig()->GetProperty("Keys", "SetSpawn");
-	tmp_prop->SetDefaultKey(CKKEY_S);
-	tmp_prop->SetComment("The key for setting spawn points (under Cheat Mode).");
-	props[1] = tmp_prop;
-	tmp_prop = GetConfig()->GetProperty("Keys", "Restart");
-	tmp_prop->SetDefaultKey(CKKEY_E);
-	tmp_prop->SetComment("The key for restarting the current level.");
-	props[2] = tmp_prop;
-	for (int i = 0; i < 3; i ++) {
-		update_key_property(i);
-	}
-	GetConfig()->SetCategoryComment("BMMO", "BMMO-related stuff for cheat detection.");
-	prop_mmo_strict_mode = GetConfig()->GetProperty("BMMO", "StrictMode");
-	prop_mmo_strict_mode->SetDefaultBoolean(false);
-	prop_mmo_strict_mode->SetComment("Completely block restarting during inappropriate conditions (for example, when on a Trafo). Otherwise only a warning will be issued.");
-	mmo_strict_mode = prop_mmo_strict_mode->GetBoolean();
+  GetConfig()->SetCategoryComment("Keys", "Set the keys used to trigger actions. Must be used with the Alt key.");
+  auto tmp_prop = GetConfig()->GetProperty("Keys", "CheatToggle");
+  tmp_prop->SetDefaultKey(CKKEY_C);
+  tmp_prop->SetComment("The key for toggling Cheat Mode.");
+  props[0] = tmp_prop;
+  tmp_prop = GetConfig()->GetProperty("Keys", "SetSpawn");
+  tmp_prop->SetDefaultKey(CKKEY_S);
+  tmp_prop->SetComment("The key for setting spawn points (under Cheat Mode).");
+  props[1] = tmp_prop;
+  tmp_prop = GetConfig()->GetProperty("Keys", "Restart");
+  tmp_prop->SetDefaultKey(CKKEY_E);
+  tmp_prop->SetComment("The key for restarting the current level.");
+  props[2] = tmp_prop;
+  for (int i = 0; i < 3; i ++) {
+    update_key_property(i);
+  }
+  GetConfig()->SetCategoryComment("BMMO", "BMMO-related stuff for cheat detection.");
+  prop_mmo_strict_mode = GetConfig()->GetProperty("BMMO", "StrictMode");
+  prop_mmo_strict_mode->SetDefaultBoolean(false);
+  prop_mmo_strict_mode->SetComment("Completely block restarting during inappropriate conditions (for example, when on a Trafo). Otherwise only a warning will be issued.");
+  mmo_strict_mode = prop_mmo_strict_mode->GetBoolean();
 }
 
 void SpeedrunShortcuts::OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) {
-	if (strcmp("Keys", category) == 0) {
-		for (int i = 0; i < 3; i++) {
-			if (prop == props[i])
-				update_key_property(i);
-		}
-	}
-	if (prop == prop_mmo_strict_mode)
-		mmo_strict_mode = prop_mmo_strict_mode->GetBoolean();
+  if (strcmp("Keys", category) == 0) {
+    for (int i = 0; i < 3; i++) {
+      if (prop == props[i])
+        update_key_property(i);
+    }
+  }
+  if (prop == prop_mmo_strict_mode)
+    mmo_strict_mode = prop_mmo_strict_mode->GetBoolean();
 }
 
 void SpeedrunShortcuts::update_key_property(int index) {
-	keys[index] = props[index]->GetKey();
+  keys[index] = props[index]->GetKey();
 }
 
 void SpeedrunShortcuts::OnProcess() {
   if (input_manager->IsKeyDown(CKKEY_LMENU)) {
     if (input_manager->IsKeyPressed(keys[0])) {
       m_bml->EnableCheat(!m_bml->IsCheatEnabled());
-			m_bml->SendIngameMessage((std::string("Cheat mode ").append(m_bml->IsCheatEnabled() ? "enabled" : "disabled")).c_str());
+      m_bml->SendIngameMessage((std::string("Cheat mode ").append(m_bml->IsCheatEnabled() ? "enabled" : "disabled")).c_str());
     }
     else if (input_manager->IsKeyPressed(keys[1])) {
       if (!m_bml->IsCheatEnabled() || !m_bml->IsIngame())
-				return;
-			if (!current_level) 
-				current_level = m_bml->GetArrayByName("CurrentLevel");
+        return;
+      if (!current_level) 
+        current_level = m_bml->GetArrayByName("CurrentLevel");
     
-			if (m_bml->IsIngame() && current_level) {
-				CK3dEntity* camRef = m_bml->Get3dEntityByName("Cam_OrientRef");
-				VxMatrix matrix = camRef->GetWorldMatrix();
-				for (int i = 0; i < 4; i++) {
-					std::swap(matrix[0][i], matrix[2][i]);
-					matrix[0][i] = -matrix[0][i];
-				}
-				current_level->SetElementValue(0, 3, &matrix);
-				m_bml->SendIngameMessage(("Set Spawn Point to ("
-																+ std::to_string(matrix[3][0]) + ", "
-																+ std::to_string(matrix[3][1]) + ", "
-																+ std::to_string(matrix[3][2]) + ")").c_str());
-			}
-		}
-		else if (input_manager->IsKeyPressed(keys[2])) {
-			if (!m_bml->IsIngame())
-				return;
-			if (mmo_client) {
-				if (mmo_client->connected() && nav_inactive && m_bml->IsPlaying()) {
-					if (mmo_strict_mode) {
-						m_bml->SendIngameMessage("Warning: you are connected to a BMMO server in Strict Mode and cannot reset inappropriately.");
-						return;
-					}
-					else {
-						bmmo::public_warning_msg msg{};
-						msg.text_content = mmo_client->get_own_name() + " just restarted a level when their ball is not controllable.";
-						msg.serialize();
-						mmo_client->send(msg.raw.str().data(), msg.size(), k_nSteamNetworkingSend_Reliable);
-					}
-				}
-			}
-			auto* esc = static_cast<CKBehaviorIO*>(m_bml->GetCKContext()->GetObject(esc_event));
-			esc->Activate();
+      if (m_bml->IsIngame() && current_level) {
+        CK3dEntity* camRef = m_bml->Get3dEntityByName("Cam_OrientRef");
+        VxMatrix matrix = camRef->GetWorldMatrix();
+        for (int i = 0; i < 4; i++) {
+          std::swap(matrix[0][i], matrix[2][i]);
+          matrix[0][i] = -matrix[0][i];
+        }
+        current_level->SetElementValue(0, 3, &matrix);
+        m_bml->SendIngameMessage(("Set Spawn Point to ("
+                                + std::to_string(matrix[3][0]) + ", "
+                                + std::to_string(matrix[3][1]) + ", "
+                                + std::to_string(matrix[3][2]) + ")").c_str());
+      }
+    }
+    else if (input_manager->IsKeyPressed(keys[2])) {
+      if (!m_bml->IsIngame())
+        return;
+      if (mmo_client) {
+        if (mmo_client->connected() && nav_inactive && m_bml->IsPlaying()) {
+          if (mmo_strict_mode) {
+            m_bml->SendIngameMessage("Warning: you are connected to a BMMO server in Strict Mode and cannot reset inappropriately.");
+            return;
+          }
+          else {
+            bmmo::public_warning_msg msg{};
+            msg.text_content = mmo_client->get_own_name() + " just restarted a level when their ball is not controllable.";
+            msg.serialize();
+            mmo_client->send(msg.raw.str().data(), msg.size(), k_nSteamNetworkingSend_Reliable);
+          }
+        }
+      }
+      auto* esc = static_cast<CKBehaviorIO*>(m_bml->GetCKContext()->GetObject(esc_event));
+      esc->Activate();
 
-			//m_bml->AddTimer(3u, [this]() {
-			std::thread([this] {
-				CKMessageManager* mm = m_bml->GetMessageManager();
-				std::this_thread::sleep_for(std::chrono::milliseconds(120));
+      //m_bml->AddTimer(3u, [this]() {
+      std::thread([this] {
+        CKMessageManager* mm = m_bml->GetMessageManager();
+        std::this_thread::sleep_for(std::chrono::milliseconds(120));
 
-				CKMessageType reset_level_msg = mm->AddMessageType("Reset Level");
-				mm->SendMessageSingle(reset_level_msg, static_cast<CKBeObject*>(m_bml->GetCKContext()->GetObjectByNameAndParentClass("Level", CKCID_BEOBJECT, nullptr)));
-				mm->SendMessageSingle(reset_level_msg, static_cast<CKBeObject*>(m_bml->GetCKContext()->GetObjectByNameAndParentClass("All_Balls", CKCID_BEOBJECT, nullptr)));
+        CKMessageType reset_level_msg = mm->AddMessageType("Reset Level");
+        mm->SendMessageSingle(reset_level_msg, static_cast<CKBeObject*>(m_bml->GetCKContext()->GetObjectByNameAndParentClass("Level", CKCID_BEOBJECT, nullptr)));
+        mm->SendMessageSingle(reset_level_msg, static_cast<CKBeObject*>(m_bml->GetCKContext()->GetObjectByNameAndParentClass("All_Balls", CKCID_BEOBJECT, nullptr)));
 
-				auto* beh = static_cast<CKBehavior*>(m_bml->GetCKContext()->GetObject(restart_level));
-				auto* output = beh->GetOutput(0);
-				output->Activate();
-			}).detach();
-		}
+        auto* beh = static_cast<CKBehavior*>(m_bml->GetCKContext()->GetObject(restart_level));
+        auto* output = beh->GetOutput(0);
+        output->Activate();
+      }).detach();
+    }
   }
 }
 
 void SpeedrunShortcuts::OnPostStartMenu() {
-	if (init)
-		return;
-	int count = m_bml->GetModCount();
-	for (int i = 0; i < count; i++) {
-		mmo_client = dynamic_cast<client*>(m_bml->GetMod(i));
-		if (mmo_client != nullptr) {
-			GetLogger()->Info("Presence of BMMO client detected, got pointer at %#010x", mmo_client);
-			break;
-		}
-	}
-	init = true;
+  if (init)
+    return;
+  int count = m_bml->GetModCount();
+  for (int i = 0; i < count; i++) {
+    mmo_client = dynamic_cast<client*>(m_bml->GetMod(i));
+    if (mmo_client != nullptr) {
+      GetLogger()->Info("Presence of BMMO client detected, got pointer at %#010x", mmo_client);
+      break;
+    }
+  }
+  init = true;
 }
--- main.h
+++ main.h
@@ -4,39 +4,39 @@
 #include "client.h"
 
 extern "C" {
-	__declspec(dllexport) IMod* BMLEntry(IBML* bml);
+  __declspec(dllexport) IMod* BMLEntry(IBML* bml);
 }
 
 class SpeedrunShortcuts : public IMod {
-	CKDataArray* current_level{};
-	InputHook* input_manager = m_bml->GetInputManager();
+  CKDataArray* current_level{};
+  InputHook* input_manager = m_bml->GetInputManager();
   CK_ID esc_event{}, restart_level{};
-	bool init = false, nav_inactive = true;
-	IProperty* props[3]{}; // cheat, spawn, reset
-	CKKEYBOARD keys[3]{}; // cheat, spawn, reset
-	client* mmo_client = nullptr;
-	IProperty* prop_mmo_strict_mode{};
-	bool mmo_strict_mode = false;
+  bool init = false, nav_inactive = true;
+  IProperty* props[3]{}; // cheat, spawn, reset
+  CKKEYBOARD keys[3]{}; // cheat, spawn, reset
+  client* mmo_client = nullptr;
+  IProperty* prop_mmo_strict_mode{};
+  bool mmo_strict_mode = false;
 
-	void update_key_property(int index);
+  void update_key_property(int index);
 
 public:
-	SpeedrunShortcuts(IBML* bml) : IMod(bml) {}
+  SpeedrunShortcuts(IBML* bml) : IMod(bml) {}
 
-	virtual CKSTRING GetID() override { return "SpeedrunShortcuts"; }
-	virtual CKSTRING GetVersion() override { return "0.1.0"; }
-	virtual CKSTRING GetName() override { return "Speedrun Shortcuts"; }
-	virtual CKSTRING GetAuthor() override { return "BallanceBug"; }
-	virtual CKSTRING GetDescription() override { return "Provides keyboard shortcuts for speedrunning."; }
-	DECLARE_BML_VERSION;
+  virtual CKSTRING GetID() override { return "SpeedrunShortcuts"; }
+  virtual CKSTRING GetVersion() override { return "0.1.0"; }
+  virtual CKSTRING GetName() override { return "Speedrun Shortcuts"; }
+  virtual CKSTRING GetAuthor() override { return "BallanceBug"; }
+  virtual CKSTRING GetDescription() override { return "Provides keyboard shortcuts for speedrunning."; }
+  DECLARE_BML_VERSION;
   
   void OnProcess() override;
-	void OnPostStartMenu() override;
+  void OnPostStartMenu() override;
   void OnLoadScript(CKSTRING filename, CKBehavior* script) override;
-	void OnBallNavInactive() override;
-	void OnBallNavActive() override;
-	void OnLoad() override;
-	void OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) override;
+  void OnBallNavInactive() override;
+  void OnBallNavActive() override;
+  void OnLoad() override;
+  void OnModifyConfig(CKSTRING category, CKSTRING key, IProperty* prop) override;
 
   // void ResetBall(CKContext* ctx);
 };
